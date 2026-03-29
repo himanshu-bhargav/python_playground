76e6e70157f040f9ab743c81ab69e75937dd2f86
Himanshu Bhargav <himanshu.bhargav@gmail.com>
2026-03-29 15:49:19 +0530

    Python Learning1 code commit

---
+++ learning1.py
@@ -0,0 +1,70 @@
+''' This file is for learning purposes only. It is not intended to be used in production. '''
+'''Print is a python function that prints the specified message to the screen.'''
+# print("Hello, World!")
+# print("This is a learning file for Python.\nIt is not intended to be used in production.")
+
+# x=10
+# print("The value of x is:", x)
+# print("The type of x is:", type(x))
+
+
+# x=10.5
+# print("The value of x is:", x)
+# print("The type of x is:", type(x))
+
+# x="Hello, World!"
+# print("The value of x is:", x)
+# print("The type of x is:", type(x))
+
+'''In Python, everything is an object. The id() function returns the identity of an object. 
+It is a unique integer that is assigned to the object when it is created. 
+The id of an object is constant during its lifetime.'''
+# x=10
+# print("The id of x is:", id(x))
+
+# x="Hello, World!"
+# y=x
+# y=50
+# print("The value of x is:", x)
+# print("The value of y is:", y)
+# print("The id of x is:", id(x))
+# print("The id of y is:", id(y)) 
+
+''' list of keywords in Python '''  
+# import keyword
+# print("The list of keywords in Python is:", keyword.kwlist)
+
+# x=100
+# print("The type of x is:", type(x))
+
+# x= 10
+# print("The type of x is:", type(x))
+
+# x=-10
+# print("The type of x is:", type(x))
+
+# x=100000000
+# print("The type of x is:", type(x))
+
+# x=2+5j
+# print("The value of x is:", x)
+# print("The type of x is:", type(x))
+# print ("the Id of x is:", id(x))
+# print ("the real part of x is:", x.real)
+# print ("the imaginary part of x is:", x.imag)
+
+# x=complex(2,5)
+# print("The value of x is:", x)
+# print("The type of x is:", type(x))
+
+# print("This is string data type in Python. It is used to store text data.\nIt is enclosed in 'single quotes', \"double quotes\" or triple quotes.")
+
+name ="My name is Himanshu Bhargav"
+print(name[0])
+print(name.upper())
+print(name.lower())     
+print(name.capitalize())
+print(name.title())
+print(name.swapcase())
+print(name.count("a"))
+print(len(name ))

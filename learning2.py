fa80b48ab101dd6ff25b7cb1443962245654c055
Himanshu Bhargav <himanshu.bhargav@gmail.com>
2026-03-30 23:22:02 +0530

    Learning2 file commit

---
+++ learning2.py
@@ -0,0 +1,64 @@
+''' This file is for learning purposes only. It is not intended to be used in production. '''
+
+# x=678
+# print("The value of x is:", x)
+# print("The type of x is:", type(x))
+
+# x=0b1010
+# print("The value of x is:", x)
+# print("The type of x is:", type(x)) 
+# print("The binary representation of x is:", bin(x))
+
+# x=345
+# print("The binary representation of x is:", bin(x))
+# print("The octal representation of x is:", oct(x))
+# print("The hexadecimal representation of x is:", hex(x))
+
+# a=10
+# b=5
+# print("The sum of a and b is:", a+b)
+# print("The difference of a and b is:", a-b)
+# print("The product of a and b is:", a*b)
+# print("The quotient of a and b is:", a/b)
+
+# print("The result of 10/3 is:", 10/3)
+# print("The remainder of 10%3 is:", 10%3)
+# print("The exponentiation of 5**2 is:", 5**2)
+# print("The exponentiation of 5**3 is:", 5**3)
+# print("The division of 5/2 is:", 5/2)
+# print("The floor division of 5//2 is:", 5//2)
+
+
+# '''In Python, we can use the assignment operator (=) to assign a value to a variable.'''
+# x=5
+# print("The value of x is:", x)
+# '''We can also use the augmented assignment operator (+=) to add a value to a variable and assign the result back to the variable.'''
+# x=x+5
+# print("The value of x is:", x)
+# x+=5
+# print("The value of x is:", x)
+
+'''We can also use the augmented assignment operator (-=) to subtract a value from a variable and assign the result back to the variable.'''
+# x=5
+# x-=5
+# print(x)
+# x*=5
+# print(x)
+# x/=5
+# print(x)
+
+
+# x=5
+# '''The augmented assignment operator (%=) is used to perform modulus operation and assign the result back to the variable.'''
+# x%=3
+# print("The value of x is:", x)
+# x=5
+# '''The augmented assignment operator (//=) is used to perform floor division and assign the result back to the variable.'''
+# x//=2
+# print("The value of x is:", x)
+
+'''The augmented assignment operator (**=) is used to perform exponentiation and assign the result back to the variable.'''
+# x=5
+# x**=2
+# print("The value of x is:", x)
+
